c72e8b7147c9f287efb0f827f8e36811114d70f0
Monika Rana <monika.rana16@gmail.com>
2018-09-01 22:33:57 +0530

    add add-note and delete-note feature

--- scripts.js
+++ scripts.js
@@ -1,24 +1,70 @@
 
+var submit = document.querySelector("input[type='submit']");
+var input = document.querySelector("input[type='text']");
+var noteTable = document.getElementById("notesTable");
+
+(function() {
+  var keys = Object.keys(localStorage);
+  keys.forEach(function(id) {
+    displayAddedNote(id);
+  });
+})();
+
 function addNote(note) {
   var noteId = "Note" + (localStorage.length+1)
   localStorage.setItem(noteId, note);
-  showNotes(noteId);
+  displayAddedNote(noteId);
+}
+function createButton(btnType, noteId){
+  console.log(btnType, noteId);
+  var btn = document.createElement('button');
+  var node = document.createTextNode(btnType);
+  btn.setAttribute("id", btnType+noteId);
+  btn.appendChild(node);
+  btn.setAttribute("onclick", "action('"+ btnType+ "','" + noteId+"')");
+  return btn;
+}
+function action(btnType, noteId) {
+  console.log("triggerd", btnType, noteId);
 }
 
-var submit = document.querySelector("input[type='submit']");
-var input = document.querySelector("input[type='text']");
-submit.addEventListener('click', function(){
+function action(btnType, noteId) {
+  if(btnType == "Edit") {
+    console.log("edit triggered for", noteId);
+  } else {
+    localStorage.removeItem(noteId);
+    var tableRow = document.getElementById(noteId);
+    tableRow.parentNode.removeChild(tableRow);
+  }
+}
+
+input.addEventListener(13, function(){
   var note = input.value;
   if(note) {
     addNote(note);
-
   } else {
     alert("No note to add");
   }
 });
-
-function showNotes(noteId) {
+submit.addEventListener('click', function(){
+  var note = input.value;
+  if(note) {
+    addNote(note);
+  } else {
+    alert("No note to add");
+  }
+});
+function displayAddedNote(noteId) {
   if(localStorage.length>0) {
-    console.log(localStorage[noteId])
+    var noteValue = localStorage[noteId];
+    var row = noteTable.insertRow(0);
+    row.setAttribute("id", noteId);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2)
+    cell1.innerHTML = noteValue;
+    cell2.appendChild(createButton("Edit", noteId));
+    cell3.appendChild(createButton("Delete", noteId))
+    console.log("note added in local storage" + noteValue);
   }
 }
